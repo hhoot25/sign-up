1f75ed7b5accd8640f121c1c557f76fb01cf8a96
hhoot25 <hhoot25@gmail.com>
2025-02-19 17:56:49 -0600

    added js to login and create btn

---
+++ index.js
@@ -0,0 +1,11 @@
+let login = document.querySelector('#login');
+
+login.addEventListener('click', ()=>{
+    alert('login clicked');
+});
+
+let create = document.querySelector('#create');
+
+create.addEventListener('click', ()=>{
+    alert('create clicked');
+});
